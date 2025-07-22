b710e7a0e60d8358757fd1c92fb0776066fec61b
J-Starling <aksenovdmitriy1006@gmail.com>
2025-07-23 01:42:15 +0300

    Add files via upload

--- server_worker.js
+++ server_worker.js
@@ -1,41 +1,75 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Кэшируем основные файлы');
+        return cache.addAll(urlsToCache).catch(err => {
+          console.error('Ошибка кэширования:', err);
+        });
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request).then(
+          response => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
 self.addEventListener('activate', event => {
-	console.log([SW]: activate)
-})
-
-self.addEventListener('install', async event => {
-	console.log([SW]: install)
-	const cache = await caches.open(staticCacheName)
-	await cache.addAll(assetUrls)
-})
-
-self.addEventListener('install', async event => {
-	console.log('Fetch', event.request)
-	event.respondWith(cacheFirst(event.request))
-	const cache = await caches.open(staticCacheName)
-	await cache.addAll(assetUrls)
-})
-
-async function cacheFirst(request) {
-	const cached = await caches.match(request)
-	return cached ?? await fetch(request)
-
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 const assetUrls = [
-	'index.html',
-	'organs.html',
-	'feedback.php',
-	'contacts.html',
-	'style.css',
-	'kidney_l.png',
-	'kidney_r.png',
-	'logo.png',
-	'dmitriy.png',
-	'organs/heart.png',
-	'organs/kidney.png',
-	'organs/intestine.png',
-	'organs/brain.png',
-	'organs/liver.png',
-	'organs/stomach.png',
-	'organs/lungs.png'
+	'/Three-Kidneys/',
+	'/Three-Kidneys/index.html',
+	'/Three-Kidneys/organs.html',
+	'/Three-Kidneys/feedback.php',
+	'/Three-Kidneys/contacts.html',
+	'/Three-Kidneys/style.css',
+	'/Three-Kidneys/kidney_l.png',
+	'/Three-Kidneys/kidney_r.png',
+	'/Three-Kidneys/logo.png',
+	'/Three-Kidneys/dmitriy.png',
+	'/Three-Kidneys/organs/heart.png',
+	'/Three-Kidneys/organs/kidney.png',
+	'/Three-Kidneys/organs/intestine.png',
+	'/Three-Kidneys/organs/brain.png',
+	'/Three-Kidneys/organs/liver.png',
+	'/Three-Kidneys/organs/stomach.png',
+	'/Three-Kidneys/organs/lungs.png'
 ]
 
-const staticCacheName = 's-app-v1'
+const CACHE_NAME = 's-app-v1'
